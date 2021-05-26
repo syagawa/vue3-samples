3dde43da6288af5115bfefdd555a56fb389ccbe6
syagawa <syagawapp@gmail.com>
2021-05-26 20:25:36 +0900

    Add eslintrc.js

---
+++ src/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  plugins: [
+    "vue"
+  ],
+  rules: {
+
+  }
+};
+
